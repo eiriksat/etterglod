09705cff693bd933a6e0c4ea94ba05f7ea552146
Eirik Sæther <eiriksat@gmail.com>
2025-10-28 23:45:17 +0100

    Add rewrite for API calls to https://api.etterglod.no

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
+import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    async rewrites() {
+        return [
+            { source: "/api/:path*", destination: "https://api.etterglod.no/api/:path*" },
+        ];
+    },
 };
 
 export default nextConfig;
